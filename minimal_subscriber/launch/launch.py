9ba3700464632d0c5bf9f2aafb1cbf26b3dcf16b
riccardik <riky.lastrico@gmail.com>
2021-05-18 18:07:54 +0200

    launch?!

--- minimal_subscriber/launch/launch.py
+++ minimal_subscriber/launch/launch.py
@@ -1,22 +1,28 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch_ros.actions import Node 
+
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+
 
 def generate_launch_description():
     return LaunchDescription([
         Node(
             package='ros1_bridge',
             executable='dynamic_bridge',
-            name='bridge_ros1'
+            name='bridge_ros1', output='screen'
         ),
         Node(
             package='examples_rclpy_minimal_subscriber',
             executable='publisher',
-            name='send_position'
+            name='send_position', output='screen'
         ),
         Node(
             package='examples_rclpy_minimal_subscriber',
             executable='subscriber',
-            name='send_position'
+            name='send_position', output='screen'
         ),
         
     ])
